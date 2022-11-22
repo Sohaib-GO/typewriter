c49243c88c4cb1ddc84b031e2f1b348b3d31dd9c
Sohaib-GO <s.almajmaie@gmail.com>
2022-11-22 13:51:37 -0700

    New line after each log

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,13 @@
 const sentence = "hello there from lighthouse labs";
 
-for (let i = 0; i < sentence.length; i++) {
-  setTimeout(() => {
-    process.stdout.write(sentence[i]);
-  }, i * 50);
+setTimeout(() => {
+  for (let i = 0; i < sentence.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+    }, i * 50);
+  }
+}, 50);
 
-}
-
+setTimeout(() => {
+  process.stdout.write("\n HI HI HI   ");
+}, 2000);
